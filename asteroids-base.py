366e08d4f8b76f878347d70a29f1c8effeb84043
Luis Filipe Carrete <lfcsanchez@live.com>
2019-04-25 16:57:40 -0300

    Criei o player

--- asteroids-base.py
+++ asteroids-base.py
@@ -24,6 +24,31 @@ YELLOW = (255, 255, 0)
 pygame.init()
 pygame.mixer.init()
 
+#Classe do jogador que representa nave.
+class Player(pygame.sprite.Sprite):
+    
+    #Construtor da classe.
+    def __init__(self):
+        #Construtor da classe pai.
+        pygame.sprite.Sprite.__init__(self)
+        
+        #Carregando a imagem do fundo
+        player_img = pygame.image.load(path.join(img_dir,"playerShip1_orange.png")).convert()
+        self.image = player_img
+        
+        #Diminui o tamanho da imagem.
+        self.image = pygame.transform.scale(player_img, (50, 38))
+        
+        #Deixando transparente.
+        self.image.set_colorkey(BLACK)
+        
+        #Detalhe sobre pocionamento.
+        self.rect = self.image.get_rect()
+        
+        #Centraliza no baixo da tela.
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+
 # Tamanho da tela.
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
@@ -57,6 +82,7 @@ try:
         # A cada loop, redesenha o fundo e os sprites
         screen.fill(BLACK)
         screen.blit(background, background_rect)
+        all_sprites.draw(screen)
         
         # Depois de desenhar tudo, inverte o display.
         pygame.display.flip()
